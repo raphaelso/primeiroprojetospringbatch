91275a95abe7395c3958650901077fc40768f27c
Raphael Oliveira <raphaelsoliveira@gmail.com>
2022-10-09 22:46:33 +0100

    [Added] - First commit, created step chunk-based

---
+++ parImparJob/src/main/java/com/roliveira/parouimpar/ParImparBatchConfig.java
@@ -0,0 +1,68 @@
+package com.roliveira.parouimpar;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.function.FunctionItemProcessor;
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class ParImparBatchConfig {
+	
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Bean
+	public Job imprimeParImparJob() {
+		return jobBuilderFactory
+				.get("imprimeParImparJob")
+				.start(imprimeParImparStep())
+				.incrementer(new RunIdIncrementer())
+				.build();
+	}
+	
+	public Step imprimeParImparStep() {
+		return stepBuilderFactory
+				.get("imprimeParImparStep")
+				.<Integer, String>chunk(1)
+				.reader(contaAteDezReader())
+				.processor(parOuImparProcessor())
+				.writer(imprimeWriter())
+				.build();
+	}
+	
+	public IteratorItemReader<Integer> contaAteDezReader() {
+		List<Integer> numerosDeUmAteDez = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+		return new IteratorItemReader<Integer>(numerosDeUmAteDez.iterator());
+	}
+	
+	public FunctionItemProcessor<Integer, String> parOuImparProcessor() {
+		return new FunctionItemProcessor<Integer, String>
+		(item -> item % 2 == 0 ? String.format("Item %s é par", item) : String.format("Item %s é ímpar", item));
+	}
+	
+	public ItemWriter<String> imprimeWriter() {
+		return itens -> itens.forEach(System.out::println);
+	}
+
+}
---
+++ parImparJob/src/test/java/com/roliveira/parouimpar/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roliveira.parouimpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
